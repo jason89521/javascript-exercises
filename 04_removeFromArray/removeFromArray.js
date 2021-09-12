63319523dcd46970521fc5169372a51ea21ed18a
jason89521 <97ssps30212@gmail.com>
2021-09-12 22:55:10 +0800

    complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+/**
+ * 
+ * @param {array} arr 
+ * @param  {...any} toBeRemoved 
+ */
+const removeFromArray = function(arr, ...toBeRemoved) {
+    let len_toBeRemoved = toBeRemoved.length;
+    for(let i = 0; i < len_toBeRemoved; i++){
+        let idx = arr.indexOf(toBeRemoved[i]);
+        if(idx !== -1)
+            arr.splice(idx, 1);
+    }
 
+    return arr;
 };
 
 // Do not edit below this line
