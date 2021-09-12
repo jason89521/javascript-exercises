9cf0a22caed9a5b4427271ae5b52ec01337af279
jason89521 <97ssps30212@gmail.com>
2021-09-12 22:36:15 +0800

    complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
 const reverseString = function(str) {
+    let strLength = str.length;
+    let result = '';
 
+    for(let i = strLength-1; i >= 0; i--) {
+        result += str[i];
+    }
+
+    return result;
 };
 
 // Do not edit below this line
