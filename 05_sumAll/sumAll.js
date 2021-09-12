95491c1f65d0519f2703c2c2a4ca06be6ad2f865
jason89521 <97ssps30212@gmail.com>
2021-09-12 23:03:30 +0800

    complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(typeof num1 !== "number" || typeof num2 !== "number"){
+        return 'ERROR';
+    }
 
+    if(num1 < 0 || num2 < 0){
+        return 'ERROR';
+    }
+
+    let abs_difference = Math.abs(num1 - num2) + 1;
+    let result = ((num1 + num2) * abs_difference) / 2;
+    return result;
 };
 
 // Do not edit below this line
