aa9d29ea0c96074419672424bcb568e89e47ee7c
jason89521 <97ssps30212@gmail.com>
2021-09-13 18:41:12 +0800

    complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
+function round(n){
+  n *= 10;
+  return Math.round(n) / 10;
+}
 
+const ftoc = function(f) {
+  let c = (f-32) * 5 / 9;
+  return round(c);
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let f = (c * 9 / 5) + 32;
+  return round(f);
 };
 
 // Do not edit below this line
