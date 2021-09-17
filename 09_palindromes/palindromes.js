839c4143c3ded0191aa72b6a1d7d733e5bd7e3d6
jason89521 <97ssps30212@gmail.com>
2021-09-17 14:27:48 +0800

    complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+/**
+ * 
+ * @param {string} str 
+ */
+const palindromes = function (str) {
+    str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let reverseStr = str.split('').reverse().join('');
+    if(reverseStr === str)
+        return true;
+    return false;
 };
 
 // Do not edit below this line
