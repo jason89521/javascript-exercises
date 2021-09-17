f710af55c8e2506fe0bc6b6ff572817e65c920fb
jason89521 <97ssps30212@gmail.com>
2021-09-17 13:28:15 +0800

    complete 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a- b;
 };
 
-const sum = function() {
-	
+/**
+ * 
+ * @param {Array} numbers 
+ * @returns 
+ */
+const sum = function(numbers) {
+	return numbers.reduce(function(prev, current){
+    return prev + current;
+  }, 0)
 };
 
-const multiply = function() {
-
+/**
+ * @param {Array} numbers
+ */
+const multiply = function(numbers) {
+  return numbers.reduce(function(prev, current){
+    return prev * current;
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let result = 1;
+  for(let i = 1; i <= n; i++)
+    result *= i;
+  return result;
 };
 
 // Do not edit below this line
