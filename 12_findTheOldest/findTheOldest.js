63d3d53cd9b42be095f3a5960620b1ac591e6247
jason89521 <97ssps30212@gmail.com>
2021-09-17 15:08:16 +0800

    complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+/**
+ * 
+ * @param {Array} people 
+ * @returns 
+ */
+const findTheOldest = function( people) {
+    people.sort(function(a, b){
+        if(!a.yearOfDeath)
+            a.yearOfDeath = (new Date()).getFullYear();
+        if(!b.yearOfDeath)
+            b.yearOfDeath = (new Date()).getFullYear();
+        const aAge = a.yearOfDeath - a.yearOfBirth;
+        const bAge = b.yearOfDeath - b.yearOfBirth;
+        return aAge > bAge ? 1 : -1;
+    });
+    return people[people.length-1];
 };
 
 // Do not edit below this line
