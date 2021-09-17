8985e9eeec0863d69018f4ea98bd6c4c66100de5
jason89521 <97ssps30212@gmail.com>
2021-09-17 14:44:55 +0800

    complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if(n < 0) return 'OOPS';
+    n = parseInt(n, 10);
+    let prev1 = 0;
+    let prev2 = 0;
+    let current = 0;
 
+    for(let i = 0; i < n; i++) {
+        if(i === 0)
+            current = 1;
+        else
+            current = prev1 + prev2;
+        
+        prev1 = prev2;
+        prev2 = current;
+    }
+    return current;
 };
 
 // Do not edit below this line
