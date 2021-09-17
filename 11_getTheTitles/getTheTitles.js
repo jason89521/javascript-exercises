0a3d0d5021cd05127427f559310b190c440e3984
jason89521 <97ssps30212@gmail.com>
2021-09-17 14:55:24 +0800

    complete 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
-const getTheTitles = function() {
-
+/**
+ * 
+ * @param {Array} books 
+ * @returns 
+ */
+const getTheTitles = function(books) { 
+    return books.reduce(function(booksTitles, book){
+        booksTitles.push(book.title);
+        return booksTitles;
+    }, []);
 };
 
 // Do not edit below this line
